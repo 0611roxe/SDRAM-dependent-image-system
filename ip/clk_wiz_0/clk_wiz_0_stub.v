54f86fd8ca0dee4c6a24537ad9a67a53202ef804
0611roxe <100147572+0611roxe@users.noreply.github.com>
2023-01-10 16:44:27 +0800

    Add files via upload

---
+++ ip/clk_wiz_0/clk_wiz_0_stub.v
@@ -0,0 +1,25 @@
+// Copyright 1986-2019 Xilinx, Inc. All Rights Reserved.
+// --------------------------------------------------------------------------------
+// Tool Version: Vivado v.2019.2 (win64) Build 2708876 Wed Nov  6 21:40:23 MST 2019
+// Date        : Mon Jan  9 22:45:28 2023
+// Host        : LAPTOP-FR39877N running 64-bit major release  (build 9200)
+// Command     : write_verilog -force -mode synth_stub
+//               e:/IP/VGA/OV5640_Video/ise/OV5640_Video/OV5640_Video.srcs/sources_1/ip/clk_wiz_0_2/clk_wiz_0_stub.v
+// Design      : clk_wiz_0
+// Purpose     : Stub declaration of top-level module interface
+// Device      : xc7z010clg400-1
+// --------------------------------------------------------------------------------
+
+// This empty module with port declaration file causes synthesis tools to infer a black box for IP.
+// The synthesis directives are for Synopsys Synplify support to prevent IO buffer insertion.
+// Please paste the declaration into a Verilog source file or add the file as an additional source.
+module clk_wiz_0(clk_out1, clk_out2, clk_out3, reset, locked, 
+  clk_in1)
+/* synthesis syn_black_box black_box_pad_pin="clk_out1,clk_out2,clk_out3,reset,locked,clk_in1" */;
+  output clk_out1;
+  output clk_out2;
+  output clk_out3;
+  input reset;
+  output locked;
+  input clk_in1;
+endmodule
