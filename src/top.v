3f0d66a79ff9d4c01d1e538396d340c278cf38f8
Frank Liu <100147572+0611roxe@users.noreply.github.com>
2023-02-04 17:52:10 +0800

    Update top.v

--- src/top.v
+++ src/top.v
@@ -8,12 +8,19 @@ module top(
 	`endif	
 	output wire				vga_hsync,
 	output wire				vga_vsync,
-	output wire [23:0]			vga_rgb
+	output wire [23:0]		vga_rgb,
+
+	output wire				ov5640_pwdn ,
+	output wire				ov5640_resetb,
+	output wire				ov5460_xclk,
+	output wire 			ov5460_iic_scl,
+	inout					ov5460_iic_sda
 );
 
 wire			clk_sys33_3m;
 wire			clk_sys100m;
 wire			clk_sys50m;
+wire			clk_sys24m;
 wire [7:0]		uart_data;
 wire			uart_flag;
 wire			vga_en;
@@ -32,6 +39,10 @@ wire [12:0]		sdram_addr;
 wire [1:0]		sdram_dqm;
 wire [23:0]		sdram_dq;
 wire			sdram_init_done;
+
+wire 			estart;
+wire [31:0]		ewdata;
+wire [7:0]		riic_data;
 
 assign vga_rgb = 	{{vga_rgb_t[15:11], vga_rgb_t[15:13]},
 					{vga_rgb_t[10:5], vga_rgb_t[10:9]},
@@ -62,6 +73,7 @@ clk_wiz_0 u_clk_wiz(
 	.clk_out1				(clk_sys33_3m),		// output clk_out1
 	.clk_out2				(clk_sys100m),		// output clk_out2
 	.clk_out3				(clk_sys50m	),		// output clk_out3
+	.clk_out4				(clk_sys24m	),
 	.reset					(~s_rst_n	), 		// input reset
 	.locked					(),					// output locked
 	.clk_in1				(sclk		)
@@ -73,21 +85,35 @@ sdram_top u_sdram_top(
 	.sdram_clk			(sdram_clk		),
 	.sdram_cke			(sdram_cke		),
 	.sdram_cs_n			(sdram_cs_n		),
-	.sdram_cas_n			(sdram_cas_n	),
-	.sdram_ras_n			(sdram_ras_n	),
+	.sdram_cas_n		(sdram_cas_n	),
+	.sdram_ras_n		(sdram_ras_n	),
 	.sdram_we_n			(sdram_we_n		),
 	.sdram_bank			(sdram_bank		),
 	.sdram_addr			(sdram_addr		),
 	.sdram_dqm			(sdram_dqm		),
 	.sdram_dq			(sdram_dq		),
-	.sdram_init_done		(sdram_init_done),
+	.sdram_init_done	(sdram_init_done),
 	.wfifo_wclk			(clk_sys50m		),
-	.wfifo_wr_en			(uart_flag		),
-	.wfifo_wr_data			({16'h0, uart_data}),
+	.wfifo_wr_en		(uart_flag		),
+	.wfifo_wr_data		({16'h0, uart_data}),
 	.rfifo_rclk			(clk_sys33_3m	),
-	.rfifo_rd_en			(vga_en			),
-	.rfifo_rd_data			(img_data		),
-	.rfifo_rd_ready			(rfifo_rd_ready	)
+	.rfifo_rd_en		(vga_en			),
+	.rfifo_rd_data		(img_data		),
+	.rfifo_rd_ready		(rfifo_rd_ready	)
+);
+
+ov5640_top u_ov5640_top(
+	.clk_sys50m			(clk_sys50m		),
+	.s_rst_n			(s_rst_n		),
+	.clk_sys24m			(clk_sys24m		),
+	.ov5640_pwdn 		(ov5640_pwdn 	),
+	.ov5640_resetb		(ov5640_resetb	),
+	.ov5460_xclk		(ov5460_xclk	),
+	.ov5460_iic_scl		(ov5460_iic_scl	),
+	.ov5460_iic_sda		(ov5460_iic_sda	),
+	.estart				(estart			),
+	.ewdata				(ewdata			),
+	.riic_data			(riic_data		)
 );
 
 endmodule
